265abbf247194762d9cc5f68c6cd25ed32f2c9ce
Joshua Lee <joshua.l@mckinneysteamacademy.org>
2024-10-19 13:44:56 -0500

    changed literally everythinf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp17011.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp17011.java
@@ -33,7 +33,10 @@ import static org.firstinspires.ftc.teamcode.Constants.climberTickPerIn;
 import static org.firstinspires.ftc.teamcode.Constants.pivotTickPerDegree;
 import static org.firstinspires.ftc.teamcode.Constants.slideTickPerIn;
 
+import android.transition.Slide;
+
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Line;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -49,33 +52,6 @@ import org.firstinspires.ftc.teamcode.roadRunnerActions.Intake;
 import java.sql.RowId;
 
 
-/*
- * This file contains an example of a Linear "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When a selection is made from the menu, the corresponding OpMode is executed.
- *
- * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
- * This code will work with either a Mecanum-Drive or an X-Drive train.
- * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
- * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
- *
- * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
- *
- * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
- * Each motion axis is controlled by one Joystick axis.
- *
- * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
- * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
- * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
- *
- * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
- * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
- * the direction of all 4 motors (see code below).
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
 
 @TeleOp(name="TeleOp17011", group="Linear OpMode")
 @Config
@@ -88,13 +64,12 @@ public class TeleOp17011 extends LinearOpMode {
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
-    private Servo Intake_Claw;
-    private Servo Intake_Spin;
-    private Servo Intake_Pivot;
-    private DcMotor Linear_Slide;
-    private DcMotor Slide_Pivot;
-    private DcMotor Right_Climber;
-    private DcMotor Left_Climber;
+    private Servo claw;
+    private Servo clawPivot;
+    private Servo specimenIntake;
+    private DcMotor leftSlide;
+    private DcMotor rightSlide;
+    private DcMotor pivot;
     private Servo Right_Hook;
     private Servo Left_Hook;
     public static double NEW_P = 10;
@@ -103,85 +78,56 @@ public class TeleOp17011 extends LinearOpMode {
 
     FtcDashboard dash;
 
-    public void Slide_Extend_High_Basket() {
+    public void slideCode() {
         if (gamepad2.x) {
-            Linear_Slide.setPower(0.7);
-            Linear_Slide.setTargetPosition((int)(31 * slideTickPerIn));
+            leftSlide.setPower(0.7);
+            leftSlide.setTargetPosition((int) (31 * slideTickPerIn));
         }
-    }
-    public void Slide_Extend_Spec_High() {
+
         if (gamepad2.a) {
-            Linear_Slide.setPower(0.7);
-            Linear_Slide.setTargetPosition((int)(20 * slideTickPerIn));
+            leftSlide.setPower(0.7);
+            leftSlide.setTargetPosition((int) (20 * slideTickPerIn));
         }
-    }
-    public void Slide_Pivot_Start() {
+
         if (gamepad2.b) {
-            Slide_Pivot.setPower(0.7);
-            Slide_Pivot.setTargetPosition(0);
+            pivot.setPower(0.7);
+            pivot.setTargetPosition(0);
         }
-    }
-    public void Slide_Pivot_Expand() {
+
         if (gamepad2.y) {
-            Slide_Pivot.setPower(0.7);
-            Slide_Pivot.setTargetPosition((int)(30 * pivotTickPerDegree));
+            pivot.setPower(0.7);
+            pivot.setTargetPosition((int) (30 * pivotTickPerDegree));
         }
-    }
-    public void Climbers_Up() {
-        if  (gamepad2.right_trigger > .01) {
-            Right_Climber.setPower(0.7);
-            Right_Climber.setTargetPosition((int)(5*climberTickPerIn));
-            Left_Climber.setPower(0.7);
-            Left_Climber.setTargetPosition((int)(5*climberTickPerIn));
-        }
-    }
-    public void Climbers_Down() {
-        if  (gamepad2.left_trigger > .01) {
-            Right_Climber.setPower(0.7);
-            Right_Climber.setTargetPosition(0);
-            Left_Climber.setPower(0.7);
-            Left_Climber.setTargetPosition(0);
-        }
-    }
-    public void Hooks_Up() {
-        if(gamepad2.dpad_up) {
+
+        if (gamepad2.dpad_up) {
             Left_Hook.setPosition(1);
             Right_Hook.setPosition(1);
         }
     }
-    public void Hooks_Down() {
-        if(gamepad2.dpad_down) {
-            Left_Hook.setPosition(1);
-            Right_Hook.setPosition(1);
-        }
-    }
-    public void Claw_Close() {
+        
+    public void intakeCode() {
         if (gamepad1.y) {
-            Intake_Claw.setPosition((int)(0));
+            claw.setPosition((int) (0));
         }
-    }
-    public void Claw_Open() {
+
         if (gamepad1.b) {
-            Intake_Claw.setPosition((int)(1));
+            claw.setPosition((int) (1));
         }
-    }
-    public void Spin_Start() {
+
         if (gamepad1.x) {
-            Intake_Spin.setPosition((int)(0));
+            clawPivot.setPosition((int) (0));
         }
-    }
-    public void Spin_Full() {
-        if (gamepad1.a)
-            Intake_Spin.setPosition((int)(1));
-    }
-    public void Intake_Pivot_Start() {
+
+        if (gamepad1.a) {
+            clawPivot.setPosition((int) (1));
+        }
+        
         if (gamepad1.right_trigger > 0.01) {
-            Intake_Pivot.setPosition(0);
+            specimenIntake.setPosition(0);
         }
-    }
-    public void Intake_Pivot_Expand() {
-        if(gamepad1.left_trigger > 0.01) {
-            Intake_Pivot.setPosition(1);
+        
+        if (gamepad1.left_trigger > 0.01) {
+            specimenIntake.setPosition(1);
         }
     }
 
@@ -202,12 +148,17 @@ public class TeleOp17011 extends LinearOpMode {
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
-        Linear_Slide = hardwareMap.get(DcMotor.class, "lift");
-        Linear_Slide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        Linear_Slide.setDirection(DcMotor.Direction.FORWARD);
+        leftSlide = hardwareMap.get(DcMotor.class, "lift");
+        leftSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftSlide.setTargetPosition(0);
+        leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftSlide.setDirection(DcMotor.Direction.FORWARD);
 
-        Slide_Pivot = hardwareMap.get(DcMotor.class, "pivot");
-        Slide_Pivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        pivot = hardwareMap.get(DcMotor.class, "pivot");
+        pivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        pivot.setTargetPosition(0);
+        pivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        pivot.setDirection(DcMotorSimple.Direction.REVERSE);
 
 //        Right_Climber = hardwareMap.get(DcMotorEx.class, "climberR");
 //        Right_Climber.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -218,11 +169,11 @@ public class TeleOp17011 extends LinearOpMode {
 //        Left_Hook = hardwareMap.get(Servo.class, "hookL");
 //        Right_Hook = hardwareMap.get(Servo.class, "hookR");
 
-        Intake_Claw = hardwareMap.get(Servo.class, "claw");
+        claw = hardwareMap.get(Servo.class, "claw");
 
-        Intake_Spin = hardwareMap.get(Servo.class, "spinX");
+        clawPivot = hardwareMap.get(Servo.class, "spinX");
 
-        Intake_Pivot = hardwareMap.get(Servo.class, "pivotY");
+        specimenIntake = hardwareMap.get(Servo.class, "pivotY");
 
 
         telemetry.addData("Status", "Initialized");
@@ -255,46 +206,8 @@ public class TeleOp17011 extends LinearOpMode {
             max = Math.max(max, Math.abs(leftBackPower));
             max = Math.max(max, Math.abs(rightBackPower));
 
-            Slide_Extend_High_Basket();
-            Slide_Extend_Spec_High();
-            Slide_Pivot_Expand();
-            Slide_Pivot_Start();
-            Intake_Pivot_Expand();
-            Intake_Pivot_Start();
-            Claw_Close();
-            Claw_Open();
-            Spin_Full();
-            Spin_Start();
-
-//            if (max > 1.0) {
-//                leftFrontPower  /= max;
-//                rightFrontPower /= max;
-//                leftBackPower   /= max;
-////                rightBackPower  /= max;
-//            }
-//
-//            if (gamepad1.x) {
-//                pidNew.p = NEW_P;
-//                pidNew.i = NEW_I;
-//                pidNew.d = NEW_D;
-//                rightBackDrive.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidNew);
-//            }
-            // This is test code:
-            //
-            // Uncomment the following code to test your motor directions.
-            // Each button should make the corresponding motor run FORWARD.
-            //   1) First get all the motors to take to correct positions on the robot
-            //      by adjusting your Robot Configuration if necessary.
-            //   2) Then make sure they run in the correct direction by modifying the
-            //      the setDirection() calls above.
-            // Once the correct motors move in the correct direction re-comment this code.
-
-            /*
-            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
-            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
-            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
-            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
-            */
+            slideCode();
+            intakeCode();
 
             // Send calculated power to wheels
             leftFrontDrive.setPower(leftFrontPower);
